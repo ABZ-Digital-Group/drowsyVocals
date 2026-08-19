a3e959635fd77ad548e2712c56f1e599fe751532
stuart Keith <stuartiek@gmail.com>
2026-08-19 20:59:41 +0100

    update

--- server.js
+++ server.js
@@ -118,5 +118,5 @@ app.get('/martial-arts', (req, res) => res.render('pages/martial-arts'));
 app.get('/online-coaching', (req, res) => res.render('pages/online-coaching'));
 app.get('/contact', (req, res) => res.render('pages/contact'));
 
-const PORT = process.env.PORT;
+const PORT = 3000;
 app.listen(PORT, '0.0.0.0', () => console.log('Listening on', PORT));
